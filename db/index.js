9c161f45f62089477514db33ed0872be9773f5f0
PilarCiganda <cigandapilar@gmail.com>
2022-10-03 16:27:04 -0300

    sacar constraseña

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const {Pool} = require("pg");
 const config = {
     user: "postgres",
     host: "localhost",
-    password: "queca914",
+    password: "",
     database: "postgres",
     port: 5432,
 }
